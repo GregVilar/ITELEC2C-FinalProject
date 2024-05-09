981d43ca3ee19717bae095bf804964ad2091786e
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2024-05-09 20:51:06 +0800

    homey

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="Home-Text">Home Page</h1>
-    </div>
+    <section className="home" id="home">
+      <div className="content">
+        <h3>
+          Serving up recipes <span>Byte by Byte</span>
+        </h3>
+        <p>
+          Where every click is a culinary adventure..
+        </p>
+        <a href="#" className="btn" id="rcpbtn">VIEW RECIPES</a>
+      </div>
+      </section>
   );
 };
 
